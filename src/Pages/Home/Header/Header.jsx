fb8c740480f29f0582ca93111ceeddbe2946c473
8881_Phan Thành Phát <phanthanhphat1.12a7.ntt@gmail.com>
2024-04-09 22:56:10 +0700

    Tao trang header

---
+++ src/Pages/Home/Header/Header.jsx
@@ -0,0 +1,34 @@
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../../Images/logo.png';
+import './Header.css';
+
+const Header = () => {
+
+
+    return (
+        <div className="head-bg">
+            <Navbar className="navbar" collapseOnSelect expand="lg">
+                <Container className="container-head">
+                    <Navbar.Brand href="/home"><img src={logo} alt="logo" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" expand="lg"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto align-items-center">
+                            <Link to="/home" className='list-item text-decoration-none'>Home</Link>
+                            <Link to="/about" className='list-item text-decoration-none'>About</Link>
+                            <Link to="/service" className='list-item text-decoration-none'>Service</Link>
+                            <Link to="/dentist" className='list-item text-decoration-none'>Dentist</Link>
+                            <Link to="/contact" className='list-item text-decoration-none'>Contact</Link>
+                            <Link to="/login" type="button" className="btn btn-danger">Login</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
