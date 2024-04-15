b2b75e122291bfc3ad2f56b8b787a47178212775
8881_Phan Thành Phát <phanthanhphat1.12a7.ntt@gmail.com>
2024-04-15 23:04:52 +0700

    fix giaodien

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Pages/Home/Header/Header';
 import Home from './Pages/Home/Home/Home.jsx';
+import Appoinment from './Pages/Home/Appoinment/Appoinment.jsx';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <Appoinment />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/home' element={<Home />} />
